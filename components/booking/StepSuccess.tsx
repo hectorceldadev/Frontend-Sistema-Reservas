31275862611d76229599b23d9c2242cd407d1408
hectorceldadev <hectorceldajusticia@outlook.com>
2026-02-07 20:37:43 +0100

    Google calendar bg-primary

--- components/booking/StepSuccess.tsx
+++ components/booking/StepSuccess.tsx
@@ -69,7 +69,7 @@ export default function StepSuccess({ booking, onClose, customerId }: StepSucces
                 href={generateGoogleCalendarUrl(booking)}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="flex items-center justify-center gap-2 py-3.5 rounded-xl border border-foreground/20 bg-secondary hover:bg-secondary transition-colors text-sm font-bold text-foreground group"
+                className="flex items-center justify-center gap-2 py-3.5 rounded-xl border border-foreground/20 bg-primary hover:bg-secondary transition-colors text-sm font-bold text-foreground group"
             >
                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 424 432">
                     <path fill="currentColor" d="M214 186v-1h201q3 12 3 36q0 93-56.5 150.5T213 429q-88 0-150.5-62T0 216T62 65T213 3q87 0 144 57l-57 56q-33-33-86-33q-54 0-92.5 39.5t-38.5 95t38.5 94.5t92.5 39q31 0 55-9.5t37.5-24.5t20.5-29.5t10-27.5H214v-74z"></path>
