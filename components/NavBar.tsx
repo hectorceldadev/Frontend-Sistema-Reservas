dc2f3ccd672fd6f3ad188e19e50f98c15d772d89
hectorceldadev <hectorceldajusticia@outlook.com>
2026-02-09 16:51:08 +0100

    All NavBar Button fixed

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -137,7 +137,7 @@ export default function Navbar({ services }: NavBarTypes) {
             {/* CTA & BURGER */}
             <div className="flex items-center gap-4">
               <button
-              onClick={openModal}
+              onClick={() => openModal()}
                 className="hidden md:flex items-center gap-2 bg-primary hover:bg-primary/90 text-white px-6 py-2.5 rounded-full ring ring-white/10 text-md font-bold transition-all shadow-[0_0_20px_rgba(var(--primary),0.3)] active:scale-95 uppercase tracking-wider"
               >
                 {navBar.cta.text}
